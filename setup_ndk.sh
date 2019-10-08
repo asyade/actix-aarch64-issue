24eb6ab957c6990746648b56d9e528a0bc784ea2
MooreDev <asya.corbeau@get-moore.com>
2019-10-08 17:05:51 +0200

    Update setup

--- setup_ndk.sh
+++ setup_ndk.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
+# Work only with recent ndk version >19
+
 set -xe
-# This script setup your environement for recent android-ndk (>28)
-# but also take care to "emulate" old ndk environement for crate that is not up-to-date
-# by creating some link to /usr/bin so crate that require old naming convention still find
-# ggc, ar, clang or strip, NOTE that gcc is aliased to clang
-# the script also create a cargo config to tell were theses binary are located
-# if you need another toolchain you must add the coresponding cargo config like bellow
-
 NDK=$HOME/android-ndk
-NDK_SYSROOT=$HOME/android-ndk/sysroot/usr
 
+NDK_SYSROOT=$HOME/android-ndk/sysroot/usr
 LINKER=$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android29-clang
 AR=$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android-ar
 STRIP=$NDK/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android-strip
